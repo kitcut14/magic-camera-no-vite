194b1896804fed868d7cb7d47c843cb3d7735e50
kitcut14 <kitcut14@gmail.com>
2025-04-04 13:25:50 +0900

    📸 update: fullscreen preview after shooting

--- sw.js
+++ sw.js
@@ -1,21 +1,27 @@
-self.addEventListener('install', e => {
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open('magic-camera').then(cache => {
+    caches.open("magic-camera").then((cache) => {
       return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './script.js',
-        './heart3.png',
-        './icon-192.png',
-        './icon-512.png',
-        './manifest.webmanifest'
-      ])
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./heart3.png",
+        "./icon-192.png",
+        "./icon-512.png",
+        "./manifest.webmanifest"
+      ]);
     })
-  )
-})
-self.addEventListener('fetch', e => {
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  )
-})
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
